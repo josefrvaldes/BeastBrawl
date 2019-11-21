7f2b5c2a71c1f547213adb538af74f7027624cec
jose valdés <josefrvaldes@gmail.com>
2019-11-21 12:12:19 +0100

    v0.01 - Comenzando facade

---
+++ src/Facade/Facade.h
@@ -0,0 +1,16 @@
+#ifndef __FACADE_H__
+#define __FACADE_H__
+
+#include <iostream>
+
+
+class Facade {
+    public:
+        Facade();
+        virtual ~Facade() = default;
+
+    private:
+};
+
+
+#endif 
---
+++ src/Facade/FacadeIrrlicht.h
@@ -0,0 +1,17 @@
+#ifndef __FACADEIRRLICHT_H__
+#define __FACADEIRRLICHT_H__
+
+#include <iostream>
+#include "Facade.h"
+
+
+class FacadeIrrlicht : public Facade {
+    public:
+        FacadeIrrlicht();
+        ~FacadeIrrlicht();
+
+    private:
+};
+
+
+#endif 
