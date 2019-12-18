68a205ac4df923ba127d7bb5b18999fde0187ef4
carlos <cdlf4@alu.ua.es>
2019-12-18 15:00:28 +0100

    v0.04 - BehaviourTree 15%

--- src/Systems/SystemBtPowerUp.cpp
+++ src/Systems/SystemBtPowerUp.cpp
@@ -14,6 +14,8 @@
 //                           COMPROBAR BEHAVIOR TREE
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// DECORATORS //////
+
+// Decorator Minimun
 struct Minimum : public Decorator {  // Tiene que intentar coger la llave 3 veces para que la pueda coger
     uint32_t totalTries = 3;
     uint32_t numTries = 0;
@@ -25,6 +27,8 @@ struct Minimum : public Decorator {  // Tiene que intentar coger la llave 3 vece
         return false;
     }
 };
+
+// Decorator Limit
 struct Limit : public Decorator {  // Decorator Limit
     uint32_t totalLimit = 3;
     uint32_t numLimit = 0;
@@ -35,6 +39,8 @@ struct Limit : public Decorator {  // Decorator Limit
         return getChild()->run(blackboard);
     }
 };
+
+// Decorator UntilFail
 struct UntilFail : public Decorator {  // Decorator UntilFail
     virtual bool run(Blackboard* blackboard) override {
         while (true) {
@@ -46,6 +52,8 @@ struct UntilFail : public Decorator {  // Decorator UntilFail
         return true;
     }
 };
+
+// Decorator Inverter
 struct Inverter : public Decorator {  // Decorator Inverter
     virtual bool run(Blackboard* blackboard) override {
         return !(getChild()->run(blackboard));
@@ -114,6 +122,36 @@ struct DontHavePoweUp : public behaviourTree {
 };
 
 
+//    None,               // 0
+//    RoboJorobo,         // 1
+//    SuperMegaNitro,     // 2
+//    PudinDeFrambuesa,   // 3
+//    EscudoMerluzo,      // 4
+//    TeleBanana,         // 5
+//    MelonMolon          // 6
+
+//Condicion -> El powerUp es Nitro, RoboJorobo, EscudoMerluzo
+struct CompPowerUp : public behaviourTree {
+    virtual bool run(Blackboard* blackboard) override {
+        auto cPowerUp = static_cast<CPowerUp*>(blackboard->actualCar->GetComponent(CompType::PowerUpComp).get());
+        if( cPowerUp->typePowerUp ==    typeCPowerUp::EscudoMerluzo || 
+            cPowerUp->typePowerUp ==    typeCPowerUp::RoboJorobo    || 
+            cPowerUp->typePowerUp ==    typeCPowerUp::SuperMegaNitro    ){
+            return true;
+        }else
+            return false;
+    }
+};
+
+
+//Condicion -> El powerUp es Nitro, RoboJorobo, EscudoMerluzo
+struct ThrowPowerUp : public behaviourTree {
+    virtual bool run(Blackboard* blackboard) override {
+        std::cout << "Lanzaaas el powerUp beibeee (realmente no)" << std::endl;
+        return true;
+    }
+};
+
 
 
 SystemBtPowerUp::SystemBtPowerUp(){
@@ -156,16 +194,32 @@ SystemBtPowerUp::SystemBtPowerUp(){
     //
 // Tenemos PU?    
 
+
+    //   CREACION DEL ARBOL DE DECISIONES
+
     selectorBehaviourTree = make_shared<selector>();
 
-    shared_ptr<HavePowerUp> s_havePowerUp = make_shared<HavePowerUp>();
-    shared_ptr<DontHavePoweUp> s_dontHavePoweUp = make_shared<DontHavePoweUp>();
+    shared_ptr<HavePowerUp> c_havePowerUp = make_shared<HavePowerUp>();             // Condicion -->    ¿Tengo un PowerUp?
+    shared_ptr<DontHavePoweUp> c_dontHavePoweUp = make_shared<DontHavePoweUp>();
 
-    shared_ptr<Inverter> m_inverter = make_shared<Inverter>();
-    m_inverter->addChild(s_havePowerUp);
+    shared_ptr<Inverter> m_inverter1 = make_shared<Inverter>();                     // Decorator -->    inverter para Condicion ¿Tengo un PowerUp?
 
-    selectorBehaviourTree->addChild(m_inverter);
-    selectorBehaviourTree->addChild(s_dontHavePoweUp);
+    shared_ptr<sequence> sequence1 = make_shared<sequence>();
+    shared_ptr<CompPowerUp> c_compPowerUp = make_shared<CompPowerUp>();             // Condicion -->    ¿El powerUp que tengo es Escudo, Nitro o Pudding?
+   // shared_ptr<ThrowPowerUp> a_throwPowerUp = make_shared<ThrowPowerUp>();           // Accion    -->    Lanzar el powerUp
+
+
+    // ASIGNACION DEL ARBOL DE DECISIONES
+
+    m_inverter1->addChild(c_havePowerUp);
+
+    selectorBehaviourTree->addChild(m_inverter1);
+    selectorBehaviourTree->addChild(sequence1);
+    selectorBehaviourTree->addChild(c_dontHavePoweUp); // hasta que acabe el arbol para imprimir una salida
+
+
+    sequence1->addChild(c_compPowerUp);
+    //sequence1->addChild(a_throwPowerUp);
 
     
 }
