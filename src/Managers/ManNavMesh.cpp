6e4c79e782767540565e62ea3c6d28c1a7304065
carlos <cdlf4@alu.ua.es>
2020-01-20 17:42:32 +0100

    v0.3 - Lectura NavmMesh

--- src/Managers/ManNavMesh.cpp
+++ src/Managers/ManNavMesh.cpp
@@ -6,11 +6,89 @@
 #include "../Components/CDimensions.h"
 #include "../Components/CCurrentNavMesh.h"
 #include "../Components/CNavMesh.h"
+#include "../../include/include_json/include_json.hpp"
+#include <cmath> 
 
 using namespace std;
-
+using json = nlohmann::json;
 
 ManNavMesh::ManNavMesh() {
+    //Leemos y añadimos los NavMesh
+    double vertex1X=0, vertex1Y=0,vertex1Z=0;   // utilizamos double porque tiene mas precison que float (64b vs 32b)
+    double vertex2X=0, vertex2Y=0,vertex2Z=0; 
+    double vertex3X=0, vertex3Y=0,vertex3Z=0; 
+    double vertex4X=0, vertex4Y=0,vertex4Z=0; 
+    vector<int> waypointsId;
+
+
+    ifstream i("data.json");
+    json j = json::parse(i);
+
+    int navMeshCount = j["NAVMESHES"].size();
+
+    std::cout << "EL NUMERO DE NAVMESH EN EL JSON ES: " << navMeshCount << std::endl;
+    //Leemos el array de NavMesh
+    for(int i = 0; i< navMeshCount; ++i){
+        string idNavMesh = to_string(i);  //El primer elemento es "NAVMESH 1"
+        auto navMeshActual = j["NAVMESHES"][idNavMesh];
+        // cogemos los 4 vertices
+        auto vertex1    = navMeshActual["vertex1"];
+            vertex1X   = vertex1["x"].get<double>();
+            vertex1Y   = vertex1["y"].get<double>();
+            vertex1Z   = vertex1["z"].get<double>();
+        auto vertex2    = navMeshActual["vertex2"];
+            vertex2X   = vertex2["x"].get<double>();
+            vertex2Y   = vertex2["y"].get<double>();
+            vertex2Z   = vertex2["z"].get<double>();
+        auto vertex3    = navMeshActual["vertex3"];
+            vertex3X   = vertex3["x"].get<double>();
+            vertex3Y   = vertex3["y"].get<double>();
+            vertex3Z   = vertex3["z"].get<double>();
+        auto vertex4    = navMeshActual["vertex4"];
+            vertex4X   = vertex4["x"].get<double>();
+            vertex4Y   = vertex4["y"].get<double>();
+            vertex4Z   = vertex4["z"].get<double>();
+
+        // una vez tenemos los 4 vertices, tenemos que sacar el centro y sus dimensiones (offset)
+        double centroNavMeshX = (vertex1X+vertex2X+vertex3X+vertex4X)/4;
+        double centroNavMeshY = (vertex1Y+vertex2Y+vertex3Y+vertex4Y)/4;
+        double centroNavMeshZ = (vertex1Z+vertex2Z+vertex3Z+vertex4Z)/4;
+        vec3 centroNavMesh = vec3(centroNavMeshX,centroNavMeshY,centroNavMeshZ); 
+        double dimensionX = (abs(centroNavMeshX - vertex1X))*2;    
+        double dimensionY = (abs(centroNavMeshY - vertex1Y))*2; 
+        double dimensionZ = (abs(centroNavMeshZ - vertex1Z))*2; 
+        // cogemos el array de wayPoints
+        auto wayPointsIdCount = navMeshActual["waypoints"].size();
+        //std::cout << "EL Navmesh " << idNavMesh << " tiene: " << wayPointsIdCount << " waypoints" << std::endl;
+        for(int k=0; k<wayPointsIdCount; k++){
+            int wayPointIdActual = navMeshActual["waypoints"][k].get<int>();
+            waypointsId.push_back(wayPointIdActual);
+            //std::cout << "meteneos waypoint con ID: " << wayPointIdActual << std::endl;
+        }
+        // POSICION , ROTACION, OFFSETX, OFFSETY, OFFSETZ, VECTOR WAYPOINTS
+        //CreateNavMesh(centroNavMesh,glm::vec3(0.0f,0.0f,0.0f),dimensionX,dimensionY,dimensionZ,waypointsId); 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // CREAMOS DE PRUEBA UN NAVMESH
     vector<int> waypoints0{0,1,2,3,4,12};
     vector<int> waypoints1{7,8,9,10,11,13};
