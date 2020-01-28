04c47191641d9b54666f96a954f0af19234500ed
jose valdés <josefrvaldes@gmail.com>
2020-01-28 19:49:11 +0100

    v0.3.0 - Intentando solucionar fallos cliente-servidor

--- src/Online/TCPClient.cpp
+++ src/Online/TCPClient.cpp
@@ -3,9 +3,9 @@
 #include <boost/bind.hpp>
 #include <deque>
 #include "../../include/include_json/include_json.hpp"
-#include "../Systems/Utils.h"
 #include "../EventManager/Event.h"
 #include "../EventManager/EventManager.h"
+#include "../Systems/Utils.h"
 
 using json = nlohmann::json;
 
@@ -15,10 +15,10 @@ using namespace boost::asio;
 using namespace std::chrono;
 using namespace std;
 
-
 TCPClient::TCPClient(string host_, string port_)
     : context{},
-      endpoints{tcp::resolver(context).resolve(host_, port_)},
+      //   endpoints{tcp::resolver(context).resolve(host_, port_)},
+      serverEndpoint{*tcp::resolver(context).resolve(tcp::v4(), host_, port_).begin()},
       stopped{false},
       socket(context),
       butler{[&]() {
@@ -26,62 +26,76 @@ TCPClient::TCPClient(string host_, string port_)
           context.run();
           cout << "///////////////////////// SALIMOS DEL CONTEXT!!! ///////////////////////////////" << endl;
       }} {
-
-    StartConnect(endpoints.begin());
+    StartConnect(serverEndpoint);
 }
 
-TCPClient::~TCPClient(){
+TCPClient::~TCPClient() {
     context.stop();
     socket.close();
-    butler.join();  
-
+    butler.join();
 }
 
-void TCPClient::Stop(){
+void TCPClient::Stop() {
     stopped = true;
 }
 
-
-
-void TCPClient::StartConnect(tcp::resolver::results_type::iterator endpoint_iter){
-    // se trata de conectar el socket
-    if (endpoint_iter != endpoints.end()){
-        socket.async_connect(endpoint_iter->endpoint(),
-            boost::bind(&TCPClient::HandleConnect,
-                this, 
-                boost::asio::placeholders::error, 
-                endpoint_iter));
-    }else{
-        std::cout << "Se cierra el socket" << std::endl;
-        Stop();
-    }
+void TCPClient::StartConnect(tcp::endpoint serverEndpoint) {
+    socket.async_connect(
+        serverEndpoint,
+        boost::bind(
+            &TCPClient::HandleConnect,
+            this,
+            boost::asio::placeholders::error));
 }
 
+void TCPClient::StartConnect(tcp::resolver::results_type::iterator endpoint_iter) {
+    // se trata de conectar el socket
+    // if (endpoint_iter != endpoints.end()) {
+    //     socket.async_connect(endpoint_iter->endpoint(),
+    //                          boost::bind(&TCPClient::HandleConnect,
+    //                                      this,
+    //                                      boost::asio::placeholders::error,
+    //                                      endpoint_iter));
+    // } else {
+    //     std::cout << "Se cierra el socket" << std::endl;
+    //     Stop();
+    // }
+}
 
-void TCPClient::HandleConnect(const boost::system::error_code& error, tcp::resolver::results_type::iterator endpoint_iter){
-    if(stopped)
+void TCPClient::HandleConnect(const boost::system::error_code& error) {
+    if (error) {
+        socket.close();
+        cout << "No se pudo conectar con el" << endl;
+    } else {
+        std::cout << "Connectado a servidor, empezamos a recibir" << endl;
+        StartReceiving();
+    }
+}
+void TCPClient::HandleConnect(const boost::system::error_code& error, tcp::resolver::results_type::iterator endpoint_iter) {
+    if (stopped) {
+        cout << "Hemos intentado HandleConnect pero el cliente tcp estaba parado" << endl;
         return;
+    }
 
-    if (!socket.is_open()){
+    if (!socket.is_open()) {
         std::cout << "El socket esta cerrado" << std::endl;
-        StartConnect(++endpoint_iter);    // probamos el siguiente endpoint disponible
-    }else if (error){
+        StartConnect(++endpoint_iter);  // probamos el siguiente endpoint disponible
+    } else if (error) {
         std::cout << "Error al conectar: " << error.message() << std::endl;
-        socket.close();                   // cerramos el socket
+        socket.close();  // cerramos el socket
         StartConnect(++endpoint_iter);
-    }else{
+    } else {
         std::cout << "Connectado a " << endpoint_iter->endpoint() << "\n";
         StartReceiving();
     }
 }
 
-
-
-
 void TCPClient::StartReceiving() {
     // udp::endpoint senderEndpoint;
     //cout << "Esperamos recibir datos" << endl;
     //std::shared_ptr<string> recevBuff = make_shared<string>();
+    cout << "Estamos en StartReceiving" << endl;
+
     std::shared_ptr<boost::array<char, 1024>> recevBuff = make_shared<boost::array<char, 1024>>();
     socket.async_receive(
         asio::buffer(*recevBuff),
@@ -93,14 +107,13 @@ void TCPClient::StartReceiving() {
             boost::asio::placeholders::bytes_transferred));
 }
 
-
-
-
-
 void TCPClient::HandleReceived(std::shared_ptr<boost::array<char, 1024>> recevBuff, const boost::system::error_code& errorCode, std::size_t bytesTransferred) {
-    if(stopped)
+    cout << "Estamos en HandleReceived" << endl;
+    if (stopped) {
+        cout << "Hemos intentado recibir pero el cliente tcp estaba parado" << endl;
         return;
-    
+    }
+
     if (!errorCode && bytesTransferred > 0) {
         string receivedString;
         std::copy(recevBuff->begin(), recevBuff->begin() + bytesTransferred, std::back_inserter(receivedString));
@@ -112,18 +125,20 @@ void TCPClient::HandleReceived(std::shared_ptr<boost::array<char, 1024>> recevBu
         json receivedJSON = json::parse(receivedString);
         uint32_t idPlayer = receivedJSON["idPlayer"];
         vector<uint32_t> arrayIdEnemies = receivedJSON["idEnemies"];
-        std::cout << "El cliente TCP recibe: "  << receivedString << std::endl;
-    } else if(errorCode) {
+        std::cout << "El cliente TCP recibe: " << receivedString << std::endl;
+    } else if (errorCode) {
         cout << "Hubo un error con código " << errorCode << endl;
+    } else {
+        cout << "No ha habido error pero bytesTransferred = 0" << endl;
     }
     StartReceiving();
 }
 
-
-
 void TCPClient::SendConnectionRequest() {
-    if(stopped)
+    if (stopped) {
+        cout << "Hemos intentado SendConnectionRequest pero el cliente tcp estaba parado" << endl;
         return;
+    }
 
     //json j;
     //j["petitionType"] = Constants::PetitionTypes::SEND_INPUTS;
@@ -146,12 +161,13 @@ void TCPClient::SendConnectionRequest() {
             boost::asio::placeholders::bytes_transferred));
 }
 
-
 void TCPClient::HandleSentConnectionRequest(const boost::system::error_code& errorCode,
-                                   std::size_t bytes_transferred) {
-    if(stopped)
+                                            std::size_t bytes_transferred) {
+    if (stopped) {
+        cout << "Hemos intentado HandleSentConnectionRequest pero el cliente tcp estaba parado" << endl;
         return;
-    
+    }
+
     if (!errorCode) {
         cout << "Mensaje enviado cliente TCP" << endl;
     } else {
--- src/State/StateLobbyMulti.cpp
+++ src/State/StateLobbyMulti.cpp
@@ -2,77 +2,64 @@
 #include <iostream>
 
 #include "../Constants.h"
-#include "../Online/TCPClient.h"
 #include "../EventManager/Event.h"
 #include "../EventManager/EventManager.h"
+#include "../Online/TCPClient.h"
 
 using namespace std;
 
-StateLobbyMulti::StateLobbyMulti() : tcpClient{make_shared<TCPClient>(SERVER_HOST, SERVER_PORT_TCP)}{
-
+StateLobbyMulti::StateLobbyMulti() : tcpClient{make_shared<TCPClient>(SERVER_HOST, SERVER_PORT_TCP)} {
     std::cout << "> LobbyMenu constructor" << std::endl;
 
     renderEngine = RenderFacadeManager::GetInstance()->GetRenderFacade();
     renderEngine->FacadeInitLobbyMulti();
-    
+
     SubscribeToEvents();
 
+    cout << "Enviamos SendConnectionRequest al server" << endl;
     tcpClient->SendConnectionRequest();
     //tcpClient->SendDateTime();
-    
 }
-
-
 
 // Cargamos los bancos de sonido Menu.
 void StateLobbyMulti::InitState() {
-    
-   // cout << "~~~ ENTRO A MENU" << endl;
-   // if (!soundEngine){
-   //     soundEngine = SoundFacadeManager::GetInstance()->GetSoundFacade();
-   //     cout << "~~~ SoundEngine en MENU es -> " << soundEngine << endl;
-   // }
-   // soundEngine->SetState(2);
-   // soundEngine->PlayEvent("Musica/menu");
+    // cout << "~~~ ENTRO A MENU" << endl;
+    // if (!soundEngine){
+    //     soundEngine = SoundFacadeManager::GetInstance()->GetSoundFacade();
+    //     cout << "~~~ SoundEngine en MENU es -> " << soundEngine << endl;
+    // }
+    // soundEngine->SetState(2);
+    // soundEngine->PlayEvent("Musica/menu");
 
     //cout << "> MENU iniciado" << endl;
 }
 
-
-void StateLobbyMulti::Render(){
+void StateLobbyMulti::Render() {
     renderEngine->FacadeDrawLobbyMulti();
 }
 
-
-void StateLobbyMulti::Input(){
+void StateLobbyMulti::Input() {
     renderEngine->FacadeCheckInputLobbyMulti();
 }
 
-
-void StateLobbyMulti::Update(){
+void StateLobbyMulti::Update() {
     EventManager::GetInstance().Update();
     //std::cout << "hola" << std::endl;
     SendData();
 }
 
-
-
-void StateLobbyMulti::SendData(){
+void StateLobbyMulti::SendData() {
     //tcpClient->SendDateTime();
 }
 
-
-void StateLobbyMulti::SubscribeToEvents(){
+void StateLobbyMulti::SubscribeToEvents() {
     EventManager::GetInstance().SuscribeMulti(Listener(
         EventType::NEW_TCP_START_MULTI,
         bind(&StateLobbyMulti::StartGameMulti, this, placeholders::_1),
         "StartGameMulti"));
-
 }
 
-
-
-void StateLobbyMulti::StartGameMulti(DataMap* d){
+void StateLobbyMulti::StartGameMulti(DataMap* d) {
     cout << "Hemos llegado a StateLobbyMulti::StartGameMulti" << endl;
     auto dataServer = any_cast<string>((*d)["dataServer"]);
     renderEngine->ThrowEventChangeToMulti(dataServer);
