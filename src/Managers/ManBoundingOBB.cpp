6a72c25e015c0e9a0263b5974343a4e1786618ee
carlos <cdlf4@alu.ua.es>
2020-02-03 12:41:49 +0100

    v0.11 - Beta con OBB real
    esta hecho de tal forma que no tiene porque ser un OBB, puede ser cualquier figura convexa que te quieras inventar beibe :)

--- src/Managers/ManBoundingOBB.cpp
+++ src/Managers/ManBoundingOBB.cpp
@@ -20,6 +20,29 @@ ManBoundingOBB::ManBoundingOBB() {
     //ManBoundingOBB(vec3(50.f, 10.f, 50.f),vec3(50.f, 10.f, 150.f),vec3(50.f, 40.f, 150.f),vec3(50.f, 40.f, 50.f));
     //ManBoundingOBB(vec3(150.f, 40.f, 50.f),vec3(150.f, 40.f, 150.f),vec3(150.f, 10.f, 150.f),vec3(150.f, 10.f, 50.f));
 
+    auto verticesOBB = new vector<vec3>;
+    verticesOBB->push_back(vec3(50.f, 40.f, 50.f));
+    verticesOBB->push_back(vec3(150.f, 40.f, 50.f));
+    verticesOBB->push_back(vec3(150.f, 10.f, 50.f));
+    verticesOBB->push_back(vec3(50.f, 10.f, 50.f));
+
+    verticesOBB->push_back(vec3(50.f, 10.f, 150.f));
+    verticesOBB->push_back(vec3 (150.f, 10.f, 150.f));
+    verticesOBB->push_back(vec3(150.f, 40.f, 150.f));
+    verticesOBB->push_back(vec3(50.f, 40.f, 150.f));
+
+    verticesOBB->push_back(vec3(50.f, 10.f, 50.f));
+    verticesOBB->push_back(vec3(50.f, 10.f, 150.f));
+    verticesOBB->push_back(vec3(50.f, 40.f, 150.f));
+    verticesOBB->push_back(vec3(50.f, 40.f, 50.f));
+
+    verticesOBB->push_back(vec3(150.f, 40.f, 50.f));
+    verticesOBB->push_back(vec3(150.f, 40.f, 150.f));
+    verticesOBB->push_back(vec3(150.f, 10.f, 150.f));
+    verticesOBB->push_back(vec3(150.f, 10.f, 50.f));
+
+    vec3 centerMass = vec3( (50+150+50+150+50+50+150+150)/8, (40+10+10+40+10+40+10+40)/8, (50+50+150+150+50+150+50+150)/8);
+
     // con 4 planos creamos un mini Cubo para cmporbar colisiones
     //CreateBoundingOBB(vec3(40.f, 40.f, 40.f),vec3(140.f, 40.f, 40.f),vec3(140.f, 10.f, 40.f),vec3(40.f, 10.f, 40.f));
     //CreateBoundingOBB(vec3(40.f, 10.f, 160.f),vec3 (140.f, 10.f, 160.f),vec3(140.f, 40.f, 160.f),vec3(40.f, 40.f, 160.f));
@@ -29,33 +52,33 @@ ManBoundingOBB::ManBoundingOBB() {
 
     //TODO: esta bien hacer ese new?????????
 
-    auto verticesOBB = new vector<vec3>;
-    verticesOBB->push_back(vec3(40.f, 40.f, 40.f));
-    verticesOBB->push_back(vec3(140.f, 40.f, 40.f));
-    verticesOBB->push_back(vec3(140.f, 10.f, 40.f));
-    verticesOBB->push_back(vec3(40.f, 10.f, 40.f));
-
-    verticesOBB->push_back(vec3(40.f, 10.f, 160.f));
-    verticesOBB->push_back(vec3 (140.f, 10.f, 160.f));
-    verticesOBB->push_back(vec3(140.f, 40.f, 160.f));
-    verticesOBB->push_back(vec3(40.f, 40.f, 160.f));
-
-    verticesOBB->push_back(vec3(30.f, 10.f, 50.f));
-    verticesOBB->push_back(vec3(30.f, 10.f, 150.f));
-    verticesOBB->push_back(vec3(30.f, 40.f, 150.f));
-    verticesOBB->push_back(vec3(30.f, 40.f, 50.f));
-
-    verticesOBB->push_back(vec3(150.f, 40.f, 50.f));
-    verticesOBB->push_back(vec3(150.f, 40.f, 150.f));
-    verticesOBB->push_back(vec3(150.f, 10.f, 150.f));
-    verticesOBB->push_back(vec3(150.f, 10.f, 50.f));
-
-    verticesOBB->push_back(vec3(30.f, 40.f, 50.f));
-    verticesOBB->push_back(vec3(40.f, 40.f, 40.f));
-    verticesOBB->push_back(vec3(40.f, 10.f, 40.f));
-    verticesOBB->push_back(vec3(30.f, 10.f, 50.f));
-
-    vec3 centerMass = vec3( (40+140+40+140+30+30+150+150+30+40)/10, (40+10+10+40+10+40+40+10+40+10)/10, (40+40+160+160+50+150+50+150+50+40)/10);
+    //auto verticesOBB = new vector<vec3>;
+    //verticesOBB->push_back(vec3(40.f, 40.f, 40.f));
+    //verticesOBB->push_back(vec3(140.f, 40.f, 40.f));
+    //verticesOBB->push_back(vec3(140.f, 10.f, 40.f));
+    //verticesOBB->push_back(vec3(40.f, 10.f, 40.f));
+//
+    //verticesOBB->push_back(vec3(40.f, 10.f, 160.f));
+    //verticesOBB->push_back(vec3 (140.f, 10.f, 160.f));
+    //verticesOBB->push_back(vec3(140.f, 40.f, 160.f));
+    //verticesOBB->push_back(vec3(40.f, 40.f, 160.f));
+//
+    //verticesOBB->push_back(vec3(30.f, 10.f, 50.f));
+    //verticesOBB->push_back(vec3(30.f, 10.f, 150.f));
+    //verticesOBB->push_back(vec3(30.f, 40.f, 150.f));
+    //verticesOBB->push_back(vec3(30.f, 40.f, 50.f));
+//
+    //verticesOBB->push_back(vec3(150.f, 40.f, 50.f));
+    //verticesOBB->push_back(vec3(150.f, 40.f, 150.f));
+    //verticesOBB->push_back(vec3(150.f, 10.f, 150.f));
+    //verticesOBB->push_back(vec3(150.f, 10.f, 50.f));
+//
+    //verticesOBB->push_back(vec3(30.f, 40.f, 50.f));
+    //verticesOBB->push_back(vec3(40.f, 40.f, 40.f));
+    //verticesOBB->push_back(vec3(40.f, 10.f, 40.f));
+    //verticesOBB->push_back(vec3(30.f, 10.f, 50.f));
+//
+    //vec3 centerMass = vec3( (40+140+40+140+30+30+150+150+30+40)/10, (40+10+10+40+10+40+40+10+40+10)/10, (40+40+160+160+50+150+50+150+50+40)/10);
 
     CreateBoundingOBB(*verticesOBB, *(&centerMass) );
 
