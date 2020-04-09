ee7c04fac60047548666c0c6fca8bc31e2fccb72
jose valdés <josefrvaldes@gmail.com>
2020-04-09 15:49:02 +0200

    v0.6.0 - Intentando integrar assimp5.0.0 en linux

--- CLEngine/MakefileWin.mk
+++ CLEngine/MakefileWin.mk
@@ -12,9 +12,9 @@ else
 	CXXFLAGS += -O3
 endif
 
-LIBS 	    	+= -L./lib/windows/glfw -lglfw3 -lglfw3dll -lopengl32 -lpthread -Wl,-rpath=./lib/windows/glfw
+LIBS 	    	+= -L./lib/windows/glfw -lglfw3 -lglfw3dll -lopengl32 -lgdi32 -lkernel32 -luser32 -lcomdlg32 -lpthread -Wl,-rpath=./lib/windows/glfw
 LIBS 	    	+= -L./lib/windows/glew -lglew32 -Wl,-rpath=lib/windows/glew
-LIBS 	    	+= -L./lib/windows/assimp -lassimp -lIrrXML -Wl,-rpath=lib/windows/assimp
+LIBS 	    	+= -L./lib/windows/assimp -lassimp.dll -lassimp -Wl,-rpath=lib/windows/assimp
 LIBS			+= -L./lib/windows/freeType2 -lfreetype -Wl,-rpath=lib/windows/freeType2
 INCLUDE     	:= -I./include -I../include -I./include/freeType2
 CC			:= x86_64-w64-mingw32-g++
