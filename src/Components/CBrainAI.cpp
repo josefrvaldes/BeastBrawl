c4b20a067e2d978040019180a3f087379c83d79a
carlos <cdlf4@alu.ua.es>
2020-05-08 17:11:06 +0200

    v0.001 - APlicada logica difusa individual
    Cada coche ahora aplica su propia aceleracion y tiene su propia velocidad

--- src/Components/CBrainAI.cpp
+++ src/Components/CBrainAI.cpp
@@ -2,6 +2,10 @@
 
 CBrainAI::CBrainAI(){
     m_compType = CompType::BrainAIComp;
+
+    //fuzzy logic del coche para el movimiento (giro y aceleracion)
+    fuzzyLogic = make_shared<SystemFuzzyLogicAI>();
+
 }
  
 
